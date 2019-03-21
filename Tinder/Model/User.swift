045921a91d915398e59c4c7125c4f4f642ecd18a
Aleksandr Avdukich <avdukichsanel@mail.ru>
2019-03-21 04:12:53 +0300

    Multiple Cards and User Model

---
+++ Tinder/Model/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Tinder
+//
+//  Created by Aleksandr Avdukich on 21/03/2019.
+//  Copyright © 2019 Sanel Avdukich. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+  //определяем св-ва для нашей модели
+  let name: String
+  let age: Int
+  let profession: String
+  let imageName: String
+  
+}
+
+
