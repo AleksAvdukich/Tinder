3a2006afc59a2d6d2982867db4b4a7429050b235
Sanel Avdukich <avdukichsanel@mail.ru>
2019-05-30 00:34:13 +0300

    MVVM Card View Model

--- Tinder/Model/User.swift
+++ Tinder/Model/User.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Sanel Avdukich. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct User {
   //определяем св-ва для нашей модели
@@ -15,6 +15,14 @@ struct User {
   let profession: String
   let imageName: String
   
+  func toCardViewModel() -> CardViewModel {
+    let attributedText = NSMutableAttributedString(string: name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
+    attributedText.append(NSAttributedString(string: "  \(age)", attributes: [.font : UIFont.systemFont(ofSize: 24, weight: .regular)]))
+    
+    attributedText.append(NSAttributedString(string: "\n\(profession)", attributes: [.font : UIFont.systemFont(ofSize: 20, weight: .regular)]))
+    
+    return CardViewModel(imageName: imageName, attributedString: attributedText, textAlignment: .left)
+  }
 }
 
 
---
+++ Tinder/ViewModels/CardViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewModel.swift
+//  Tinder
+//
+//  Created by Aleksandr Avdukich on 29/05/2019.
+//  Copyright © 2019 Sanel Avdukich. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+  let imageName: String
+  let attributedString: NSAttributedString
+  let textAlignment: NSTextAlignment
+}
+
+
