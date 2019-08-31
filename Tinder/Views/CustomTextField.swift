ee871c4be928e199c507067d51c02a390faa33c4
Sanel Avdukich <avdukichsanel@mail.ru>
2019-08-31 22:18:58 +0300

    Creating a Registration Page

---
+++ Tinder/Views/CustomTextField.swift
@@ -0,0 +1,37 @@
+//
+//  CustomTextField.swift
+//  Tinder
+//
+//  Created by Aleksandr Avdukich on 31/08/2019.
+//  Copyright © 2019 Sanel Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    
+    init(padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        layer.cornerRadius = 25
+    }
+    
+    //перемещение текста в текстовом поле до последнего символа
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
