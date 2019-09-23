79719d2ad7c38a9ae221cdad41c20ca531f8c70e
Sanel Avdukich <avdukichsanel@mail.ru>
2019-09-23 15:27:20 +0300

    Stretchy Header UIScrollView

--- Tinder/Controllers/User Details/UserDetailsController.swift
+++ Tinder/Controllers/User Details/UserDetailsController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class UserDetailsController: UIViewController {
+class UserDetailsController: UIViewController, UIScrollViewDelegate {
 
+    lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.alwaysBounceVertical = true //эффект опускания фото
+        sv.contentInsetAdjustmentBehavior = .never //фото занимает верхнюю панель айфона где FaceID
+        sv.delegate = self //замыкание надо сделать lazy var
+        return sv
+    }()
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "User name 30\nDoctor\nSome bio text down below"
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.fillSuperview()
+        
+        scrollView.addSubview(imageView)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        
+        scrollView.addSubview(infoLabel)
+        infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16))
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
     }
     
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let changeY = -scrollView.contentOffset.y
+        var width = view.frame.width + changeY * 2
+        width = max(view.frame.width, width)
+        imageView.frame = CGRect(x: min(0, -changeY), y: min(0, -changeY), width: width, height: width) //эффект растягивания фото
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.dismiss(animated: true)
+    }
+    
 }
